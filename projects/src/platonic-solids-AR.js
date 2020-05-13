5297266b3937abf7448dfba060ef3e86ceaf66b1
lucasDinizCosta <lucasdinizcostaprofessional@gmail.com>
2020-05-13 20:09:07 -0300

    repositioning of the platonic solids - AR

--- projects/src/platonic-solids-AR.js
+++ projects/src/platonic-solids-AR.js
@@ -29,7 +29,7 @@ function main(){
 	renderer.domElement.style.top = '0px';
 	renderer.domElement.style.left = '0px';
     renderer.setPixelRatio(window.devicePixelRatio);  //Improve Ratio of pixel in function of the of device
-    renderer.setSize( window.innerWidth, window.innerHeight );  //640, 480
+    renderer.setSize(window.innerWidth, window.innerHeight);  //640, 480
 
     // Adiciona a saída do renderizador para um elemento da página HTML
     document.getElementById("webgl-output").appendChild(renderer.domElement);
@@ -39,13 +39,14 @@ function main(){
     axes.name = "AXES";
     axes.visible = false;
     scene.add(axes);
+
+    /*var groundPlane = createGroundPlane(1, 1,0); // width and height
+    groundPlane.rotateX(degreesToRadians(-90));
+	scene.add(groundPlane);*/
 	
 	// Object Material for all objects -- MeshNormalMaterial
     var objectMaterial = new THREE.MeshBasicMaterial({color:"rgb(255, 0, 0)"});
     objectMaterial.side = THREE.DoubleSide;     
-
-    // Height of objects
-    var heightObjects = 0.35;   //0.25
 
     // Add objects to scene
     var objectArray = new Array();
@@ -53,8 +54,8 @@ function main(){
     criationObjects();
 
     function criationObjects(){
-        scene.add(createTetrahedron(0.28, 0));
-        scene.add(createCube(0.28));
+        scene.add(createTetrahedron(0.35, 0));
+        scene.add(createCube(0.50));
         scene.add(createOctahedron(0.28, 0));
         scene.add(createDodecahedron(0.28, 0));
         scene.add(createIcosahedron(0.28, 0));
@@ -76,6 +77,11 @@ function main(){
 
         this.wireframe = false;
         this.color = "rgb(255, 0, 0)";
+
+        // Rotation
+        /*this.rotX = 0.1;
+        this.rotY = 0.1;
+        this.rotZ = 0.1;*/
 
         // Geometry
         this.mesh = objectArray[0];
@@ -154,6 +160,17 @@ function main(){
     guiFolder.add(controls, "axesSize", 0.1, 3).listen().onChange(function(e){
          controls.updateAxes();
     });
+
+    /*guiFolder.add(controls, "rotX", -60, 60).listen().onChange(function(e){
+        controls.mesh.rotation.x = (degreesToRadians(controls.rotX));
+    });
+    guiFolder.add(controls, "rotY", -60, 60).listen().onChange(function(e){
+        controls.mesh.rotation.y = (degreesToRadians(controls.rotY));
+    });
+    guiFolder.add(controls, "rotZ", -60, 60).listen().onChange(function(e){
+        controls.mesh.rotation.z = (degreesToRadians(controls.rotZ));
+    });*/
+
     guiFolder.addColor(controls, 'color').onChange(function(e){
         controls.updateColor();
     });
@@ -171,8 +188,13 @@ function main(){
     {
         var geometry = new THREE.TetrahedronGeometry(radius, detail);
         var object = new THREE.Mesh(geometry, objectMaterial);
-        object.position.set(0.0, heightObjects, 0.0);
-        object.rotation.y = 10;
+        object.position.set(0.0, radius/3, 0.0);      //Color Axe (Red, Green, Blue)
+
+        //Set rotation
+        object.rotation.x = degreesToRadians(-41.12);
+        object.rotation.y = degreesToRadians(-42.26);
+        object.rotation.z = degreesToRadians(0.1);
+
         object.visible = false;
         object.name = "Tetrahedron";
 
@@ -193,13 +215,16 @@ function main(){
     {
         let geometry = new THREE.BoxGeometry(s, s, s);
         let object = new THREE.Mesh(geometry, objectMaterial);
-        object.position.set(0.0, heightObjects, 0.0);
+        object.position.set(0.0, s/2, 0.0);
+        //object.rotation.x = 2;          // in Radians
+        //object.rotation.y = 1;
+        //
         object.visible = false;
         object.name = "Cube";
 
         // Border
         var geo = new THREE.EdgesGeometry( object.geometry );
-        var mat = new THREE.LineBasicMaterial( { color: 0x000000, transparent: true, opacity: 0.4 } );
+        var mat = new THREE.LineBasicMaterial( { color: 0x000000, transparent: true, opacity: 0.4});
         var borderLine = new THREE.LineSegments( geo, mat );
         borderLine.renderOrder = 1; // make sure wireframes are rendered 2nd
         borderLine.name = "borderLine";
@@ -214,7 +239,7 @@ function main(){
     {
         var geometry = new THREE.OctahedronGeometry(radius, detail);
         var object = new THREE.Mesh(geometry, objectMaterial);
-        object.position.set(0.0, heightObjects, 0.0);
+        object.position.set(0.0, radius, 0.0);
         object.visible = false;
         object.name = "Octahedro";
 
@@ -235,7 +260,7 @@ function main(){
     {
         var geometry = new THREE.DodecahedronGeometry(radius, detail);
         var object = new THREE.Mesh(geometry, objectMaterial);
-        object.position.set(0.0, heightObjects, 0.0);
+        object.position.set(0.0, radius, 0.0);
         object.visible = false;
         object.name = "Dodecahedron";
 
@@ -256,7 +281,7 @@ function main(){
     {
         let geometry = new THREE.IcosahedronGeometry(radius, detail);
         let object = new THREE.Mesh(geometry, objectMaterial);
-        object.position.set(0.0, heightObjects, 0.0);
+        object.position.set(0.0, radius - 0.05, 0.0);
         object.visible = false;
         object.name = "Icosahedron";
 
@@ -290,12 +315,13 @@ function main(){
 		// sourceUrl : THREEx.ArToolkitContext.baseURL + '../data/videos/headtracking.mp4',
 
 		// resolution of at which we initialize the source image
-		//sourceWidth: 640,
-		//sourceHeight: 480,
+		//sourceWidth: 640,   //640
+		//sourceHeight: 480,  // 480
 
 		// resolution displayed for the source
-		// displayWidth: window.innerWidth,  //window.innerWidth,
-		// displayHeight: window.innerHeight//window.innerHeight
+		//displayWidth: 640,  //window.innerWidth,
+        //displayHeight: 480//window.innerHeight
+        
 	})
 
 	arToolkitSource.init(function onReady(){
@@ -357,7 +383,10 @@ function main(){
 		// patternUrl : THREEx.ArToolkitContext.baseURL + '../data/data/patt.kanji',
 		// as we controls the camera, set changeMatrixMode: 'cameraTransformMatrix'
 		changeMatrixMode: 'cameraTransformMatrix'
-	});
+    });
+    
+    console.log(arToolkitContext);
+    console.log(arToolkitSource);
 
 	// as we do changeMatrixMode: 'cameraTransformMatrix', start with invisible scene
 	scene.visible = false;
