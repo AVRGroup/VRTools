75aa3f84436ac0ff4dd8b94fff53d86f67c48229
lucasDinizCosta <lucasdinizcostaprofessional@gmail.com>
2020-09-03 01:34:58 -0300

    Inclined Plane(AR) - Adjust the forces diagram

--- projects/src/inclined-plane-AR.js
+++ projects/src/inclined-plane-AR.js
@@ -89,6 +89,12 @@ function main() {
             informations: false,
         },
 
+        // Collision Check
+        collision:{
+            ramp: false,
+            ground: false,
+        },
+
         createRamp: function(){
             for(let i = 0; i < this.ramp.length; i++){
                 scene.remove(this.ramp[i]);         // Remove old version
@@ -108,6 +114,7 @@ function main() {
             ramp_material.map.wrapT = THREE.RepeatWrapping;
             
             var ramp = new Physijs.BoxMesh(new THREE.BoxGeometry(this.lengthRamp, 0.01, this.widthRamp), ramp_material, 0);
+            ramp.name = "ramp";
 
             let altura = Math.sin(this.angleRamp * (Math.PI/180)) * this.lengthRamp;
             let fixDistRamp = 0.025;
@@ -292,24 +299,47 @@ function main() {
             scene.add(this.mesh);
 
             handleCollision = function( collided_with, linearVelocity, angularVelocity ) {
-                if(collided_with.name === "ground"){
-                    controls.groupForces.children[0].visible = false;
-                    controls.groupForces.children[1].visible = true;
-                    controls.groupForces.children[2].visible = false;
-                    //console.log("Chao");
+                if(controls.frictionBox > 0){             // Without friction
+                    if(collided_with.name === "ground"){ 
+                        controls.collision.ground = true;
+                        controls.groupForces.children[0].visible = false;
+                        controls.groupForces.children[1].visible = true;
+                        controls.groupForces.children[2].visible = false;
+                        controls.groupForces.children[3].visible = false;
+                        //console.log("Chao");
+                    }
+                    if(collided_with.name === "ramp" && controls.collision.ground === false){ //FIX the diagram forces
+                        controls.collision.ramp = true;
+                        controls.groupForces.children[0].visible = true;
+                        controls.groupForces.children[1].visible = false;
+                        controls.groupForces.children[2].visible = false;
+                        controls.groupForces.children[3].visible = false;
+                        //console.log("Rampa");
+                    }
                 }
                 else{
-                    controls.groupForces.children[0].visible = true;
-                    controls.groupForces.children[1].visible = false;
-                    controls.groupForces.children[2].visible = false;
-                    //console.log("Rampa");
+                    if(collided_with.name === "ground"){ 
+                        controls.collision.ground = true;
+                        controls.groupForces.children[0].visible = false;
+                        controls.groupForces.children[1].visible = false;
+                        controls.groupForces.children[2].visible = false;
+                        controls.groupForces.children[3].visible = true;
+                        //console.log("Chao");
+                    }
+                    if(collided_with.name === "ramp" && controls.collision.ground === false){ //FIX the diagram forces
+                        controls.collision.ramp = true;
+                        controls.groupForces.children[0].visible = false;
+                        controls.groupForces.children[1].visible = false;
+                        controls.groupForces.children[2].visible = true;
+                        controls.groupForces.children[3].visible = false;
+                        //console.log("Rampa");
+                    }
                 }
                 this.collisions++;
             }
             this.mesh.collisions = 0;
             this.mesh.addEventListener( 'collision', handleCollision );
 
-            createAxisOnObject(this.mesh, this.lengthBox);     // Put center axis on object
             this.groupForces = createForcesDiagram(controls, this.lengthBox);             // id to identify collision and plot the forces
             this.groupForces.rotation.y = THREE.MathUtils.degToRad(90);
             scene.add(this.groupForces);
@@ -337,6 +367,13 @@ function main() {
 
             document.getElementById("alertPanel").style.display = "none";
             this.animation = true;
+            this.collision.ramp = false;
+            this.collision.ground = false;
+
+            this.groupForces.children[0].visible = false;
+            this.groupForces.children[1].visible = false;
+            this.groupForces.children[2].visible = false;
+            this.groupForces.children[3].visible = false;
         },
 
         updateForces: function(){
@@ -345,9 +382,26 @@ function main() {
                 this.groupForces.position.y = this.mesh.position.y + 0.2;//this.mesh.position.y + this.lengthBox + 0.2; 
                 this.groupForces.position.z = this.mesh.position.z;
             }
+
+            // Forces Diagram -- Stopped Box
+            if(this.collision.ground){
+                if(Math.abs(this.mesh.getLinearVelocity().x) < 0.001 && Math.abs(this.mesh.getLinearVelocity().y) < 0.001
+                && Math.abs(this.mesh.getLinearVelocity().z) < 0.001){
+                this.groupForces.children[0].visible = false;
+                this.groupForces.children[1].visible = false;
+                this.groupForces.children[2].visible = false;
+                this.groupForces.children[3].visible = true;
+                }
+            }
         },
 
         updateDates: function(){
+            this.collision.ramp = false;
+            this.collision.ground = false;
+            this.groupForces.children[0].visible = false;
+            this.groupForces.children[1].visible = false;
+            this.groupForces.children[2].visible = false;
+            this.groupForces.children[3].visible = false;
             updateInstructionPanel(gravity, this);
         },
     };
@@ -387,13 +441,11 @@ function main() {
     var objectMenu = gui.addFolder("Menu");
     objectMenu.open();
     objectMenu.add(controls, "frictionBox", 0, 1, 0.01).name("Friction").onChange(function(e){
-        controls.animation = false;
         controls.createRamp();           // Recria o objeto pois a fisica é mudada
         controls.createBox();            
         controls.updateDates();
     });
     objectMenu.add(controls, "angleRamp", 10, 50, 2).name("Angle (°)").onChange(function(e){
-        controls.animation = false;
         controls.createRamp();           // Recria o objeto pois a fisica é mudada
         controls.createBox();           
         controls.updateDates();
@@ -521,9 +573,8 @@ function main() {
         controls.updateForces();
         
         if(controls.animation){
-            //scene.simulate(undefined, 2);     // Fix the error that occurrent when change parameter the block
+            scene.simulate(undefined, 2);     // Fix the error that occurrent when change parameter the block
                                             // start with an initial speed 
-            scene.simulate(undefined, 2);
         }
         renderer.render(scene, camera);
         requestAnimationFrame(render);
@@ -541,14 +592,12 @@ function createForcesDiagram(controls, size){
     centerDiagram.position.x = 0;
     centerDiagram.position.z = 0;
     centerDiagram.rotation.z = THREE.MathUtils.degToRad(controls.angleRamp);
-
-    //let headLength = // Default: 0.2 * lenght of line 
+  
     size = size/3;
   
     // Axes of origin of block
     var groupForces = new THREE.Group;
     groupForces.name = "Forces";
-    groupForces.add(centerDiagram);
   
                 /**************
                  * Com atrito *
@@ -561,12 +610,10 @@ function createForcesDiagram(controls, size){
     var dir = new THREE.Vector3(0, 1, 0 );
     dir.normalize();  //normalize the direction vector (convert to vector of length 1)
     var origin = new THREE.Vector3(0, 0, 0);
-    var length = size + 1;
+    var length = size + 2;
     var hex = 0xff0000;
-    var arrowHelper = new THREE.ArrowHelper( dir, origin, length, hex);
+    var arrowHelper = new THREE.ArrowHelper( dir, origin, length, hex );
     arrowHelper.rotation.z = THREE.MathUtils.degToRad(180 - controls.angleRamp);
-    //arrowHelper.line.material.linewidth = 5;
-    //console.log(arrowHelper);
     centerDiagram.add(arrowHelper);
   
     /************
@@ -576,7 +623,7 @@ function createForcesDiagram(controls, size){
     dir = new THREE.Vector3(0, 1, 0 );
     dir.normalize();  //normalize the direction vector (convert to vector of length 1)
     origin = new THREE.Vector3(0, 0, 0);
-    length = size + 1;
+    length = size + 2;
     hex = 0x00ff00;
     arrowHelper = new THREE.ArrowHelper( dir, origin, length, hex );
     centerDiagram.add(arrowHelper);
@@ -587,15 +634,16 @@ function createForcesDiagram(controls, size){
     dir = new THREE.Vector3(1, 0, 0);
     dir.normalize(); //normalize the direction vector (convert to vector of length 1)
     origin = new THREE.Vector3(0, 0, 0);
-    length = size + 1;
+    length = size + 2;
     hex = 0x0000ff;
     arrowHelper = new THREE.ArrowHelper( dir, origin, length, hex );
     centerDiagram.add(arrowHelper);
+  
     groupForces.add(centerDiagram);
   
-              /**
-               * Sem atrito
-               */
+      /**********************************
+     * Colisão com o chão de madeira   *
+     **********************************/
   
     centerDiagram = new THREE.Mesh(new THREE.SphereGeometry(0.015, 64, 64), block_material);
     centerDiagram.position.y = 0;
@@ -610,7 +658,7 @@ function createForcesDiagram(controls, size){
     var dir = new THREE.Vector3(0, 1, 0 );
     dir.normalize();  //normalize the direction vector (convert to vector of length 1)
     var origin = new THREE.Vector3(0, 0, 0);
-    var length = size + 0.5;
+    var length = size + 2;
     var hex = 0xff0000;
     var arrowHelper = new THREE.ArrowHelper( dir, origin, length, hex );
     arrowHelper.rotation.z = THREE.MathUtils.degToRad(180);
@@ -623,17 +671,71 @@ function createForcesDiagram(controls, size){
     dir = new THREE.Vector3(0, 1, 0 );
     dir.normalize();  //normalize the direction vector (convert to vector of length 1)
     origin = new THREE.Vector3(0, 0, 0);
-    length = size + 0.5;
+    length = size + 2;
     hex = 0x00ff00;
     arrowHelper = new THREE.ArrowHelper( dir, origin, length, hex );
     centerDiagram.add(arrowHelper);
+  
+    /************
+     *  Atrito  *
+     ***********/
+    dir = new THREE.Vector3(1, 0, 0);
+    dir.normalize(); //normalize the direction vector (convert to vector of length 1)
+    origin = new THREE.Vector3(0, 0, 0);
+    length = size + 2;
+    hex = 0x0000ff;
+    arrowHelper = new THREE.ArrowHelper( dir, origin, length, hex );
+    centerDiagram.add(arrowHelper);
+  
     groupForces.add(centerDiagram);
   
-            /**********************************
-             * Colisão com o chão de madeira  *
-             **********************************/
-    
-    centerDiagram = new THREE.Mesh(new THREE.SphereGeometry(0.5, 64, 64), block_material);
+            /**************
+             * Sem atrito *
+             **************/
+  
+    /**********
+     * Ramp   *
+     **********/
+      
+    centerDiagram = new THREE.Mesh(new THREE.SphereGeometry(0.015, 64, 64), block_material);
+    centerDiagram.position.y = 0;
+    centerDiagram.position.x = 0;
+    centerDiagram.position.z = 0;
+    centerDiagram.visible = false;
+    centerDiagram.rotation.z = THREE.MathUtils.degToRad(controls.angleRamp);
+  
+     /**********
+     *  Peso  *
+     *********/
+  
+    var dir = new THREE.Vector3(0, 1, 0 );
+    dir.normalize();  //normalize the direction vector (convert to vector of length 1)
+    var origin = new THREE.Vector3(0, 0, 0);
+    var length = size + 2;
+    var hex = 0xff0000;
+    var arrowHelper = new THREE.ArrowHelper( dir, origin, length, hex );
+    arrowHelper.rotation.z = THREE.MathUtils.degToRad(180 - controls.angleRamp);
+    centerDiagram.add(arrowHelper);
+  
+    /************
+     *  Normal  *
+     ************/
+  
+    dir = new THREE.Vector3(0, 1, 0 );
+    dir.normalize();  //normalize the direction vector (convert to vector of length 1)
+    origin = new THREE.Vector3(0, 0, 0);
+    length = size + 2;
+    hex = 0x00ff00;
+    arrowHelper = new THREE.ArrowHelper( dir, origin, length, hex );
+    centerDiagram.add(arrowHelper);
+  
+    groupForces.add(centerDiagram);
+  
+      /**********************************
+     * Colisão com o chão de madeira   *
+     **********************************/
+  
+    centerDiagram = new THREE.Mesh(new THREE.SphereGeometry(0.015, 64, 64), block_material);
     centerDiagram.position.y = 0;
     centerDiagram.position.x = 0;
     centerDiagram.position.z = 0;
@@ -646,9 +748,10 @@ function createForcesDiagram(controls, size){
     var dir = new THREE.Vector3(0, 1, 0 );
     dir.normalize();  //normalize the direction vector (convert to vector of length 1)
     var origin = new THREE.Vector3(0, 0, 0);
-    var length = size + 0.5;
+    var length = size + 2;
     var hex = 0xff0000;
     var arrowHelper = new THREE.ArrowHelper( dir, origin, length, hex );
+    arrowHelper.rotation.z = THREE.MathUtils.degToRad(180);
     centerDiagram.add(arrowHelper);
   
     /************
@@ -658,79 +761,15 @@ function createForcesDiagram(controls, size){
     dir = new THREE.Vector3(0, 1, 0 );
     dir.normalize();  //normalize the direction vector (convert to vector of length 1)
     origin = new THREE.Vector3(0, 0, 0);
-    length = size + 0.5;
+    length = size + 2;
     hex = 0x00ff00;
     arrowHelper = new THREE.ArrowHelper( dir, origin, length, hex );
     centerDiagram.add(arrowHelper);
-  
-    /************
-     *  Atrito  *
-     ***********/
-    dir = new THREE.Vector3(1, 0, 0);
-    dir.normalize(); //normalize the direction vector (convert to vector of length 1)
-    origin = new THREE.Vector3(0, 0, 0);
-    length = size + 0.5;
-    hex = 0x0000ff;
-    arrowHelper = new THREE.ArrowHelper( dir, origin, length, hex );
-    centerDiagram.add(arrowHelper);
+    
     groupForces.add(centerDiagram);
+  
     return groupForces;
-}
-  
-/**************************************************
- * Eixos de orientação do objeto                  *
- * @param {*} object                              *
- * @param {*} size                                *
- **************************************************/
-  
-function createAxisOnObject(object, size){
-    size = size/2;
-  
-    // Axes of origin of block
-    var groupAxis = new THREE.Group;
-    groupAxis.name = "Axis";
-    groupAxis.visible = false;
-    object.add(groupAxis);
-  
-    // X
-    var dir = new THREE.Vector3(1, 0, 0 );
-  
-    //normalize the direction vector (convert to vector of length 1)
-    dir.normalize();
-  
-    var origin = new THREE.Vector3(0, 0, 0);
-    var length = size + 0.5;
-    var hex = 0xff0000;
-  
-    var arrowHelper = new THREE.ArrowHelper( dir, origin, length, hex );
-    groupAxis.add(arrowHelper);
-  
-    // Y
-    dir = new THREE.Vector3(0, 1, 0 );
-  
-    //normalize the direction vector (convert to vector of length 1)
-    dir.normalize();
-  
-    origin = new THREE.Vector3(0, 0, 0);
-    length = size + 0.5;
-    hex = 0x00ff00;
-  
-    arrowHelper = new THREE.ArrowHelper( dir, origin, length, hex );
-    groupAxis.add(arrowHelper);
-  
-    // Z
-    dir = new THREE.Vector3(0, 0, 1);
-  
-    //normalize the direction vector (convert to vector of length 1)
-    dir.normalize();
-  
-    origin = new THREE.Vector3(0, 0, 0);
-    length = size + 0.5;
-    hex = 0x0000ff;
-  
-    arrowHelper = new THREE.ArrowHelper( dir, origin, length, hex );
-    groupAxis.add(arrowHelper);
-}
+  }
   
 /**********************************************
  * Adiciona uma caixa de madeira na cena      *
