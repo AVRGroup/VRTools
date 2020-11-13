c63d44baed377fba00182f2c36a73ac33f197605
André Luiz Cunha de Oliveira <andre_sfmg123@hotmail.com>
2020-11-13 15:05:07 -0300

    aneurysm model position fixed

--- projects/src/vascular-diseases.js
+++ projects/src/vascular-diseases.js
@@ -76,14 +76,14 @@ function init() {
     stroke = ASSETS.objects.stroke;
     stroke.scale.set(0.45, 0.45, 0.45);
     stroke.position.set(0, 15, 0);
-    stroke.rotation.set(0, -Math.PI / 12, 0)
+    stroke.rotation.set(0, -Math.PI / 12, 0);
     stroke.visible = true;
     content.stroke.model = stroke;
     scene.add(stroke);
 
     aneurysm = ASSETS.objects.aneurysm;
-    aneurysm.scale.set(180, 180, 180);
-    aneurysm.position.set(-10, -30, 30);
+    aneurysm.scale.set(200, 200, 200);
+    aneurysm.position.set(-5, -35, -25);
     aneurysm.visible = false;
     content.aneurysm.model = aneurysm;
     scene.add(aneurysm)
@@ -102,7 +102,6 @@ function init() {
     thrombus.visible = false;
     content.thrombus.model = thrombus;
     scene.add(thrombus)
-
 
     highlight = ASSETS.objects.highlight;
     highlight.position.set(-3.5, -2, 7.5);
@@ -177,8 +176,8 @@ const content = {
         <a href="https://www.heart.org/en/health-topics/aortic-aneurysm/what-is-an-aneurysm" target="_blank" rel="noopener external">Heart.org</a>`,
         credits: '<a href="https://sketchfab.com/3d-models/abdominal-aortic-aneurysm-e951550381ad49739a38f9ffb2370899" target="_blank" rel="noopener external">laurenwahl</a> (adapted)',
         highlight: {
-            position: new THREE.Vector3(-8.2, 3.7, 20),
-            scale: new THREE.Vector3(10, 10, 10),
+            position: new THREE.Vector3(-3, 2, 2),
+            scale: new THREE.Vector3(10.2, 10.2, 10.2),
             pointView: new THREE.Vector3(2, -10, -30),
         }
     },
